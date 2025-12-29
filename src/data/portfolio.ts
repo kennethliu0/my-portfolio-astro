fd645c7d5bb11da16f317628c42f7e7d5f36e85b
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2025-12-29 16:01:24 -0600

    Shorten summary

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -127,4 +127,4 @@ export const socialLinks: SocialLinks = {
   resume: '/Kenneth_Liu_resume.pdf'
 };
 
-export const about = "I'm a computer science student at Stanford and a full-stack developer dedicated to building high-impact software. I am an engineering team member at Carta, a data-driven course planning site for Stanford students. I'm also currently a Software Engineer Intern at LastingLearn, where I'm helping build an AI-native SAT test preparation platform. I'm passionate about clean code, performance, and creating seamless user experiences. In my free time, I enjoy going to the gym and listening to R&B and pop.";
+export const about = "I'm a computer science student at Stanford and a full-stack developer. I am an engineering team member at Carta, a data-driven course planning site for Stanford students. I'm also currently a Software Engineer Intern at LastingLearn, helping build an AI-native SAT test preparation platform. I'm passionate about clean code, performance, and creating seamless user experiences.";
